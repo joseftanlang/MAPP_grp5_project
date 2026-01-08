72e295d4c52fe0dd62ba4f0873856d45489f2027
Josef Tan <tanjosef03@gmail.com>
2026-01-08 15:28:27 +0800

    Fix nested git repository issue

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(mapp_app());
+}
+
+class mapp_app extends StatelessWidget {
+  const mapp_app({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return mapp_app();
+  }
+}
